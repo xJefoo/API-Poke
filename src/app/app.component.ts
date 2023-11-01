c542d4bec6472401afe2b56bd93ebcc316dffc2c
Jeferson Nascimento <jeferson.pereiran@gmail.com>
2023-11-01 08:04:20 -0300

    adiciona imagens e botoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+declare var require: any;
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=11000';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'frontAddress';
+
+  Pokedex = require('pokeapi-js-wrapper');
+
+  // onChoose(event?: MouseEvent) {
+  //   this.Pokedex = new this.Pokedex.Pokedex();
+  //   const evtMsg = event
+  //     ? ' Event target class is ' + (event.target as HTMLElement).className
+  //     : // with await, be sure to be in an async function (and in a try/catch)
+  //       (async () => {
+  //         const Pokemon = await this.Pokedex.getPokemonByName('golduck');
+  //         debugger;
+  //         console.log(Pokemon);
+  //       })();
+  // }
+  pokeAdd(event?: MouseEvent) {
+    this.Pokedex = new this.Pokedex.Pokedex();
+    debugger;
+    var newpoke = document.getElementById('poke-autocomplete');
+    // document.getElementById("a").innerHTML ="";
+    // = "<img src=\"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/" + newpokeValue + ".gif\">";
+  }
 }
